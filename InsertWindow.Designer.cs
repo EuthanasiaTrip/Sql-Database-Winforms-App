d2ac7040fe7d7b36e89d979ea971896b08dfb769
Матвей <craftyea@gmail.com>
2021-05-23 21:08:04 +0300

    setting all things up

---
+++ InsertWindow.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class InsertWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tableName
+            // 
+            this.tableName.AutoSize = true;
+            this.tableName.Location = new System.Drawing.Point(12, 9);
+            this.tableName.Name = "tableName";
+            this.tableName.Size = new System.Drawing.Size(81, 17);
+            this.tableName.TabIndex = 0;
+            this.tableName.Text = "TableName\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InsertWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(570, 467);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InsertWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавить информацию в таблицу";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tableName;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Queries Hell.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    internal class ToolStripMenuItemWithSQL : ToolStripMenuItem
+    {
+        public string SQLquery { get; set; }
+    }
+    public partial class MainWindow : Form
+    {
+        private string[] queriesList = new string[]
+        {
+            //2
+            "select Цена, Название from products_view",
+            //3
+            "select Цена, Название from products_view order by Название",
+            //4
+            "select Цена, Название as Товар from products_view where Цена > 3000",
+            //5
+            "select Цена, Название as Товар from products_view where Цена between 3000 and 10000",
+            //6
+            "select * from products_view where Название like 'М%'",
+            //7
+            "select avg(`Цена` * product_amount) as `Средняя стоимость товаров` from products_view",
+            //8
+            "select* from products where `Название` = {}",
+            //9
+            "select costumers.Организация, products.Название " +
+            "from applications inner join products on applications.`Товар` = products.id " +
+            "inner join costumers on applications.`Покупатель` = costumers.id where costumers.имя = 'Андрей'",
+            //10
+            "select(Цена* product_amount) as `Стоимость товара` from products_view",
+            //11
+            "select costumers.Организация, sum(applications.`Требуемое количество` * products.Цена) as `Сумма` " +
+            "from applications inner join products on applications.`Товар` = products.id" +
+            " inner join costumers on applications.`Покупатель` = costumers.id "+
+            "group by costumers.Организация",
+            //12
+            /*"select costumers.Организация, max(applications.`Требуемое количество` * products.Цена) as `Стоимость` " +
+            "from applications inner join products on applications.`Товар` = products.id" +
+            " inner join costumers on applications.`Покупатель` = costumers.id " +
+            "where sum(applications.`Требуемое количество` * products.Цена) = (select max(applications.`Требуемое количество` * products.Цена)" +
+            " from applications inner join products on applications.`Товар` = products.id) " +
+            "group by costumers.Организация",*/
+
+            /*"select s.Организация, s.Sum from (select costumers.Организация as Организация, sum(applications.`Требуемое количество` * products.Цена) as Sum " +
+            "from applications inner join products on applications.`Товар` = products.id" +
+            " inner join costumers on applications.`Покупатель` = costumers.id "+
+            "group by costumers.Организация) s where s.Sum = (select max(applications.`Требуемое количество` * products.Цена) as MaxSum " +
+            "from applications inner join products on applications.`Товар` = products.id)",*/
+
+            "select costumers.Организация, sum(applications.`Требуемое количество` * products.Цена) as `Сумма` " +
+            "from applications inner join products on applications.`Товар` = products.id" +
+            " inner join costumers on applications.`Покупатель` = costumers.id "+
+            "group by costumers.Организация order by Сумма desc limit 1",
+            //13
+            "select costumers.Фамилия, costumers.Имя, costumers.Отчество, max(applications.`Требуемое количество`)" +
+            " from applications inner join products on applications.`Товар` = products.id " +
+            "inner join costumers on applications.`Покупатель` = costumers.id where products.Название = 'Молоко'",
+            //14
+            "select costumers.Фамилия, costumers.Имя, costumers.Отчество, products.Название, " +
+            "applications.`Требуемое количество`, products.product_amount from applications " +
+            "inner join products on applications.`Товар` = products.id " +
+            "inner join costumers on applications.`Покупатель` = costumers.id where applications.`Требуемое количество` > products.product_amount",
+            //15
+            "select* from costumers where Адрес is null",
+            //16
+            "select products.`Название`, applications.`Требуемое количество`, (applications.`Требуемое количество` * products.Цена) as `Стоимость` " +
+            "from applications inner join products on applications.`Товар` = products.id " +
+            "inner join costumers on applications.`Покупатель` = costumers.id",
+            //17
+            "select* from applications where day(`Дата размещения заявки`) = 1",
+            //18
+            "select costumers.id, applications.`Номер заявки`, (applications.`Требуемое количество` * products.Цена) " +
+            "from applications inner join products on applications.`Товар` = products.id " +
+            "inner join costumers on applications.`Покупатель` = costumers.id",
+            //19
+            "select applications.`Номер заказа`",
+            //20
+            "select costumers.`Организация`, (applications.`Требуемое количество` * products.Цена) as `Сумма`, " +
+            "applications.`Дата размещения заявки` from applications " +
+            "inner join products on applications.`Товар` = products.id " +
+            "inner join costumers on applications.`Покупатель` = costumers.id",
+            //21
+            "select * from app_view where `Дата оплаты` is null"
+
+    };
+        private ToolStripMenuItemWithSQL[] premadeQueries = new ToolStripMenuItemWithSQL[20];
+
+        private void drawPremadeQuery(object sender, EventArgs e)
+        {
+            isPrmdQuerySelected = true;
+            currentTable.Name = null;
+            currentTable.View = null;
+
+            toolStripMenuItem2.Checked = false;
+            toolStripMenuItem3.Checked = false;
+            toolStripMenuItem4.Checked = false;
+
+            set_table_name(null);
+
+            ToolStripMenuItemWithSQL n = (ToolStripMenuItemWithSQL)sender;
+            drawTable(n.SQLquery);
+        }
+
+    }
+}
